c0b0e6d5e2a82ea154765d1527b1e1c2273cd847
Romeu Miranda Borges <romeuborges19@gmail.com>
2024-02-22 17:50:21 -0300

    fix: organiza arquivos

--- api/handlers.go
+++ api/handlers.go
@@ -2,40 +2,11 @@ package api
 
 import (
 	"classroom/dto"
-	"encoding/json"
-	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
-type apiError struct {
-	Err string
-	Status int
-}
-
-func (e apiError) Error() string {
-	return e.Err
-}
-
-type apiFunc func(http.ResponseWriter, *http.Request) error
-
-func MakeHTTPHandler(f apiFunc) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		if err := f(w, r); err != nil {
-			if e, ok := err.(apiError); ok {
-				writeJSON(w, e.Status, e)
-			}
-			writeJSON(w, http.StatusMethodNotAllowed, apiError{Err: "Internal server error."})
-		}
-	}
-}
-
-func writeJSON(w http.ResponseWriter, status int, v any) error {
-	w.WriteHeader(status)
-	w.Header().Add("Content-Type", "application/json")
-	return json.NewEncoder(w).Encode(v)
-}
 
 func (s *Server) handleGetCourses(w http.ResponseWriter, r *http.Request) error {
 	if r.Method != http.MethodGet {
---
+++ api/utils.go
@@ -0,0 +1,35 @@
+package api
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type apiError struct {
+	Err string
+	Status int
+}
+
+func (e apiError) Error() string {
+	return e.Err
+}
+
+type apiFunc func(http.ResponseWriter, *http.Request) error
+
+func MakeHTTPHandler(f apiFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if err := f(w, r); err != nil {
+			if e, ok := err.(apiError); ok {
+				writeJSON(w, e.Status, e)
+			}
+			writeJSON(w, http.StatusMethodNotAllowed, apiError{Err: "Internal server error."})
+		}
+	}
+}
+
+func writeJSON(w http.ResponseWriter, status int, v any) error {
+	w.WriteHeader(status)
+	w.Header().Add("Content-Type", "application/json")
+	return json.NewEncoder(w).Encode(v)
+}
+
